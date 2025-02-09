ceef7120f240490466119ae3e347f5c453c7f5e8
Idriss Chefai <111351441+Idriss-Chefai@users.noreply.github.com>
2025-02-09 16:47:28 +0100

    Add files via upload

---
+++ src/main/java/utils/MyDB.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDB {
+    private String url = "jdbc:mysql://localhost:3307/3a21_codex_pi";
+    private String user = "root";
+    private String password = "";
+    private Connection conn;
+    private static MyDB instance;
+
+    public static MyDB getInstance() {
+        if (instance == null) {
+            instance = new MyDB();
+        }
+        return instance;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    private MyDB() {
+        try {
+            this.conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection established");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
