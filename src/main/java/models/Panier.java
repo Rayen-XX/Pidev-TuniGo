2d473ac96c6823929518fdcace2b62f1744ff4e5
marzoukiyosr <marzouki.yosr@esprit.tn>
2025-02-12 00:02:57 +0100

    crud implémenté

--- src/main/java/models/Panier.java
+++ src/main/java/models/Panier.java
@@ -5,14 +5,61 @@ public class Panier {
     private double prix_total;
 
     public Panier() {}
-    public Panier(int id, int id_equip, int qte_com, double prix_total) {
+
+    public Panier(int id, int id_equip,int id_utilisateur ,int qte_com, double prix_total) {
         this.id = id;
         this.id_equip = id_equip;
+        this.id_utilisateur=id_utilisateur;
         this.qte_com = qte_com;
         this.prix_total = prix_total;
     }
+    public Panier(int id_equip,int id_utilisateur,int qte_com, double prix_total) {
 
+        this.id_equip = id_equip;
+        this.id_utilisateur=id_utilisateur;
+        this.qte_com = qte_com;
+        this.prix_total = prix_total;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId_equip() {
+        return id_equip;
+    }
+    public void setId_equip(int id_equip) {
+        this.id_equip = id_equip;
+    }
+    public int getQte_com() {
+        return qte_com;
+    }
+    public void setQte_com(int qte_com) {
+        this.qte_com = qte_com;
+    }
+    public double getPrix_total() {
+        return prix_total;
+    }
+    public void setPrix_total(double prix_total) {
+        this.prix_total = prix_total;
 
+    }
+    public int getId_utilisateur() {
+        return id_utilisateur;
+    }
+    public void setId_utilisateur(int id_utilisateur) {
+        this.id_utilisateur = id_utilisateur;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Panier{" +
+                "id=" + id +
+                ", id_equip=" + id_equip +
+                ", qte_com=" + qte_com +
+                ", id_utilisateur=" + id_utilisateur +
+                ", prix_total=" + prix_total +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 import models.Equipement;
+import models.Panier;
 import services.EquipementService;
+import services.PanierService;
 import utils.MyDB;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,22 +10,35 @@ import utils.MyDB;
 public class Main {
     public static void main(String[] args) {
 
-        //MyDB.getInstance();
+
         EquipementService equipement1 = new EquipementService();
+        PanierService panier1 = new PanierService();
         try {
-            Equipement equip1 = new Equipement(20, 10, "lunette", 120.50, "image.jpg");
+            Equipement equip1 = new Equipement(24, 10, "lunette", 120.50, "image.jpg");
+
+            Panier panier = new Panier(23,1,10,200000);
+            panier1.create(panier);
+            panier1.update(panier);
+            panier1.delete(panier);
+            System.out.println(panier1.getAll());
+
+
+
+
+
             //Equipement equip2 = new Equipement(1, 10, "tente", 120.50, "image.jpg");
             //System.out.println(equip.toString());
             //equipement1.create(equip1);
             //equipement1.create(equip2);
-            System.out.println("ajout avec succes");
+            //System.out.println("ajout avec succes");
             //equipement1.update(equip1);
             //System.out.println("update avec succes");
             //System.out.println(equipement1.getAll());
 
 
-            equipement1.delete(equip1);
-            System.out.println("equipement supprimé");
+            /*equipement1.delete(equip1);
+            System.out.println("equipement supprimé");*/
+
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
